859c3809bb109bc6a767a6d59348e93d681773a7
haydenf022 <haydog2006@gmail.com>
2025-06-03 14:23:24 -0400

    added dark theme, button variant

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
-import { Logos } from "./components/Logos.tsx";
+import { Logos } from "./components/logos.tsx";
 import { Button } from "./components/ui/button.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="bg-gray-700 h-screen w-screen">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex flex-col items-center">
         <Logos />
-        <Button onClick={() => setCount(() => count + 1)}>
+        <Button variant="secondary" onClick={() => setCount(() => count + 1)}>
           Counter is at {count}
         </Button>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
